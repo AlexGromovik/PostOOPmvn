28ea1d4b54303f406d314323427a8f2410a6e10b
Alex Gromovik <alexzevs@yandex.ru>
2024-05-27 22:19:38 +1000

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String pasport;
+    public String phoneNum;
+    public boolean subscription;
+    public FormDate birthday;
+}
